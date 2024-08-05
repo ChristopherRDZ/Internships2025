91a335aea050672dc244dc7421feb15059689225
Christopher Gael Contla Rodriguez <cgcr1502@gmail.com>
2024-08-04 19:16:12 -0600

    Update firebaseConfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
 // firebaseConfig.js
 const admin = require('firebase-admin');
-const serviceAccount = require('./internshipslist-firebase-adminsdk-41apm-08ba72ea4e.json'); // Cambia esta ruta
+const serviceAccount = require('./internshipslist-firebase-adminsdk-41apm-08ba72ea4e.json'); 
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://internshipslist.firebaseio.com' // Cambia <your-project-id>
+    databaseURL: 'https://internshipslist.firebaseio.com'
 });
 
 module.exports = admin;
